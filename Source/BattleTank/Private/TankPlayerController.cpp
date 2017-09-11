8b45cdf0c978f4eb39040c4b6c3f0343cc74515c
Omar Wasfi <31694021+omerwasfi@users.noreply.github.com>
2017-09-11 14:23:27 +0200

    002-Create a GetControlledTank function
    Create BeginPlay function , you will get a report about the stats of the Tank if it found or not

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,8 +3,17 @@
 #include "TankPlayerController.h"
 
 ATank* ATankPlayerController::GetControlledTank() const {
+
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::BeginPlay(){
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+		UE_LOG(LogTemp, Error, TEXT("Player Controller not possesing a tank ! "))
+	else
+		UE_LOG(LogTemp,Warning,TEXT("Player Controller possessing: %s"), *ControlledTank->GetName())
+}
 
 
